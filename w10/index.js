b8b9f5aadff3452df7ab792985237a2369078716
Tri Dang <dangtrantri@gmail.com>
2024-05-08 21:32:55 +0700

    Week 10 solutions

---
+++ w10/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// to get data sent via <form method="post">
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// session related settings - working with session
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+
+app.use(session({
+  store: new FileStore,
+  secret: "keyboard hero",
+  resave: true,
+  saveUninitialized: true
+}));
+
+// view related settings
+const path = require("path");
+app.engine(".html", require("ejs").__express);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// data
+const products = require("./products").products;
+
+// home page
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// search page
+app.get("/search", (req, res) => {
+  let displayProducts = products;
+  if (req.query.q) {
+    const name = req.query.q;
+    displayProducts = products.filter((p) => {
+      return p.name.startsWith(name);
+    });
+  }
+  res.render('search', {
+    products: displayProducts
+  });
+});
+
+// single product page
+app.get("/product/:id", (req, res) => {
+  const id = req.params.id;
+  const product = products.find((p) => p.id == id);
+  res.render("product", {
+    product: product
+  });
+});
+
+// protected page - only allow access if logged in
+app.get("/account", (req, res) => {
+  if (req.session.username) {
+    res.render("account", {
+      username: req.session.username
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+// login page
+app.get("/login", (req, res) => {
+  if (req.session.username) {
+    return res.redirect("/account");
+  }
+  res.render("login");
+});
+
+// login logic
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (password == "COSC2430") {
+    req.session.username = username;
+    res.redirect("/account");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get('*', (req, res) => {
+  res.send('Not found 404');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ w10/products.js
@@ -0,0 +1,13 @@
+const products = [
+  {id: 1, name: "Mouse", price: 20},
+  {id: 2, name: "Keyboard", price: 10},
+  {id: 3, name: "LCD", price: 150},
+  {id: 4, name: "Speaker", price: 200},
+  {id: 5, name: "SSD Disk", price: 240},
+  {id: 6, name: "TouchPad", price: 140},
+  {id: 7, name: "Camera", price: 150},
+];
+
+module.exports = {
+  products
+}
